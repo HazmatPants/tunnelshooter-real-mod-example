bf3350fe425386724d8ddb8611593b1a9cc76507
HazmatPants <hazmatpants@gmail.com>
2025-12-17 23:59:27 +0000

    add more info to main.gd

---
+++ README.md
@@ -0,0 +1,16 @@
+# TunnelShooter: Real Example Mod
+
+This repo contains an example mod for [tunnelshooter-real](https://github.com/HazmatPants/tunnelshooter-real)
+
+Mods use GDScript for scripting, the language used in Godot Engine, and the language TunnelShooter: Real was written in.
+
+# WARNING
+Mods are able to:
+- Run abitrary code
+- Access your files (with FileAcess, DirAccess, OS)
+- Make HTTP requests (with HTTPClient)
+- Crash the game
+
+If you download a mod, please review its code first.
+
+[scripts/main.gd](main.gd) contains a lot of info for available functions and variables.
--- scripts/main.gd
+++ scripts/main.gd
@@ -3,14 +3,79 @@ extends BaseMod
 var sound
 
 func on_load():
+    # This runs when the mod is loaded
     sound = load_ogg(mod_path, "assets/sounds/test.ogg")
     print("Test Mod loaded!")
     GLOBAL.playsound(sound)
 
 func on_ready():
+    # This runs when the mod is added to the scene tree (`func _ready()`)
     print("Test Mod ready!")
 
-#func _unhandled_input(event: InputEvent) -> void:
-#    if event is InputEventKey:
-#        if event.keycode == KEY_H and event.pressed:
-#            GLOBAL.playsound(sound)
+func on_unload():
+    # This will run when the mod is unloaded (currently not implemented)
+    print("Test Mod unloaded!")
+
+# You can run ANY GDScript code.
+
+# The game has a few auto-loads for easier interaction
+# `GLOBAL`:
+#    GLOBAL.playsound(stream, volume_linear, pitch_scale, bus)
+#      stream: the AudioStream to play
+#      volume_linear: the sound's linear volume
+#          0.0 is silent, 1.0 is normal volume.
+#          defaults to 1.0
+#      pitch_scale: the sound's pitch multiplier.
+#          defaults to 1.0
+#      bus: the bus to play the sound on. defaults to the "SFX" bus
+#    GLOBAL.playsound3d(stream, global_position, volume_linear, pitch_scale, bus)
+#      same as GLOBAL.playsound, but in 3D.
+#      specify `global_position` for where to play the sound in 3D space
+#    GLOBAL.randsfx(sound_list)
+#      returns a random AudioStream from an Array of AudioStreams
+# `GunManager`:
+#    register_gun(
+#     gun_id: String,
+#     gun_name: String,
+#     caliber_id: String,
+#     scene_path: String,
+#     description: String="",
+#     manufacturer: String="unknown",
+#     )
+#       registers a new gun for use in-game
+#       gun_id: the gun's internal identifier
+#       gun_name: the gun's display gun_name, shows in the inspect screen
+#       caliber_id: the internal id of the round the gun uses
+#       scene_path: a path to the gun's scene file (.tscn)
+#       description: a description for the gun, shows in the inspect screen
+#       manufacturer: the gun's manufacturer, just shows in the inspect screen
+
+# BaseMod also contains some helper functions and variables
+#   Functions:
+#       load_wav(base_path: String, file_path: String) -> AudioStreamWAV
+#           loads a WAV audio file and returns it
+#       load_ogg(base_path: String, file_path: String) -> AudioStreamOggVorbis:
+#           loads an OGG Vorbis audio file and returns it
+#       load_image(base_path: String, file_path: String) -> Image:
+#           loads an Image and returns it
+#   Variables:
+#       var mod_path: String
+#           the path to the mod's folder
+#       var mod_id: String
+#           the mod's id
+#       var mod_name: String
+#           the mod's display name
+#       var author: String
+#           the mod's author
+#       var description: String
+#           the mod's description
+#       var priority: int
+#           the mod's load priority'
+#       var main: String
+#           the path to the mod's main script file
+
+# This function plays a sound when you press the H key
+func _unhandled_input(event: InputEvent) -> void:
+    if event is InputEventKey:
+        if event.keycode == KEY_H and event.pressed:
+            GLOBAL.playsound(sound)
